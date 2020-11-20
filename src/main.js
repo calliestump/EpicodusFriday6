6fa1cd339468af7222138c3b24491b04061550bc
Callie Stump <callie@stu.mp>
2020-11-20 18:40:54 -0500

    removes API data variables

--- src/main.js
+++ src/main.js
@@ -14,30 +14,23 @@ $(document).ready(function() {
     event.preventDefault();
     $(".conversionResult").show();
     let userAmount = $("#usAmount").val();
-    //userAmount.toFixed(2);
-    //const usdRate = 1;
     let exgRate = $("#exchangeTo").val();
     clear();
     let promise = Exchange.getConversion(exgRate);
     promise.then(function(resolvedResponse) {
       const body = JSON.parse(resolvedResponse);
-      const canadian = body.conversion_rates.CAD;
-      const ruble = body.conversion_rates.RUB;
-      const ringgit = body.conversion_rates.MYR;
-      const sol = body.conversion_rates.PEN;
-      const lira = body.conversion_rates.TRY;
       if (exgRate == "CAD") {
-        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is $${parseInt(userAmount * canadian).toFixed(2)}`);
+        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is $${parseInt(userAmount * body.conversion_rates.CAD).toFixed(2)}`);
       } else if (exgRate == "RUB") {
-        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is ₽${parseInt(userAmount * ruble).toFixed(2)}`);
+        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is ₽${parseInt(userAmount * body.conversion_rates.RUB).toFixed(2)}`);
       } else if (exgRate == "MYR") {
-        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is RM/${parseInt(userAmount * ringgit).toFixed(2)}`);
+        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is RM/${parseInt(userAmount * body.conversion_rates.MYR).toFixed(2)}`);
       } else if (exgRate == "PEN") {
-        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is S/${parseInt(userAmount * sol).toFixed(2)}`);
+        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is S/${parseInt(userAmount * body.conversion_rates.PEN).toFixed(2)}`);
       } else if (exgRate == "TRY") {
-        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is ₺${parseInt(userAmount * lira).toFixed(2)}`);
+        $(".convertedTotal").text(`$${userAmount} in ${exgRate} is ₺${parseInt(userAmount * body.conversion_rates.TRY).toFixed(2)}`);
       } else if (exgRate == "none") {
-        alert("Please make a selection");
+        $(".convertedTotal").text("Please select a valid conversion.");
       }     
     }, 
     function(rejectedResponse) {
